ae8cd497b8612d1472b88298face91d1a0cc6509
Vijayakumar <msvijayakumar1>
2023-03-04 06:25:40 -0800

    initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://magenta-pasca-4adc10.netlify.app',
     methods: ['GET', 'POST']
   }
 })
